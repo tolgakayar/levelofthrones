19271c02529501d9f594d13b2a119d48f03a76d4
Mac <mac@Macs-MacBook-Pro.local>
2017-08-19 16:52:42 +0300

    login form

--- LifeLevel/ViewController.swift
+++ LifeLevel/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
